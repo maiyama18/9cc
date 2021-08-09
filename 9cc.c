de0b67cb8f6a1dc6648dbaa4cd077c0bcdfc9b66
maiyama18 <myam0118@gmail.com>
2021-08-09 14:29:08 +0900

    add addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -6,10 +6,31 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "arg missing!\n");
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        switch (*p) {
+        case '+':
+            p++;
+            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        case '-':
+            p++;
+            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        default:
+            fprintf(stderr, "invalid char: %c\n", *p);
+            exit(1);
+        }
+    }
+
     printf("\tret\n");
+
+    return 0;
 }
