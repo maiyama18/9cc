bab2e7fd3cd34fa6eebe87fd5889d486bb392cc3
maiyama18 <myam0118@gmail.com>
2021-08-09 15:48:19 +0900

    add tokenizer and allow space between tokens

--- 9cc.c
+++ 9cc.c
@@ -1,33 +1,127 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
 
-int main(int argc, char *argv[]) {
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token* next;
+    int val;
+    char* str;
+};
+
+// Current token.
+Token* token;
+
+void error(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+Token* new_token(TokenKind kind, Token* cur, char* str) {
+    Token* t = calloc(1, sizeof(Token));
+    t->kind = kind;
+    t->str = str;
+
+    cur->next = t;
+    return t;
+}
+
+// Tokenine all the input and return the first token.
+Token* tokenize(char* p) {
+    Token head;
+    head.next = NULL;
+    Token* cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("failed to tokenize: %s\n", p);
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
+
+// If the next token is expected reserved, return true and comsume a token.
+// Otherwise return false.
+bool consume_reserved(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) return false;
+    token = token->next;
+    return true;
+}
+
+// If the next token is expected reserved, comsume a token.
+// Otherwise report error.
+void expect_reserved(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error("expected %s but got %s", op, token->str[0]);
+    token = token->next;
+}
+
+// If next token is number, return its value and consume a token.
+// Otherwise report error.
+int expect_number() {
+    if (token->kind != TK_NUM)
+        error("expect number, but got %d\n", token->kind);
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_end() {
+    return token->kind == TK_EOF;
+}
+
+int main(int argc, char* argv[]) {
     if (argc < 2) {
         fprintf(stderr, "arg missing!\n");
     }
 
-    char *p = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("\n");
     printf("main:\n");
-    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p) {
-        switch (*p) {
-        case '+':
-            p++;
-            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+    printf("\tmov rax, %d\n", expect_number());
+
+    // output assembly with consuming succesive tokens with form '+ <NUM>' or '- <NUM>'
+    while (!at_end()) {
+        if (consume_reserved('+')) {
+            printf("\tadd rax, %d\n", expect_number());
             continue;
-        case '-':
-            p++;
-            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        default:
-            fprintf(stderr, "invalid char: %c\n", *p);
-            exit(1);
         }
+
+        expect_reserved('-');
+        printf("\tsub rax, %d\n", expect_number());
     }
 
     printf("\tret\n");
